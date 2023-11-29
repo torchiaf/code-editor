8c925fd94074a738085175ed2f81061ab0f675f2
Francesco Torchia <francesco.torchia@suse.com>
2023-11-29 12:53:59 +0100

    Enable https & certificates
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

---
+++ scripts/generate-certificates.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout charts/code-editor/tls.key -out charts/code-editor/tls.crt -subj "/C=XX/ST=Italy/L=Empoli/O=SUSE/OU=ECM/CN=code-editor"
+
+echo "Self-signed certificate successfully generated."
