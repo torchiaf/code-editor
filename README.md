8ec1e984e9c8775c92d6fcf2523ab25db4f51752
Francesco Torchia <francesco.torchia@suse.com>
2023-12-04 21:34:21 +0100

    Generate routes dinamically
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ code-server running on Kubernetes with Rest API authentication and multi-user su
   ```bash
   npm install
   npm run cluster:create
+  kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v2.10/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
+  kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v2.10/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml
   npm run code-editor:install
   ```
 - Authenticate a user:
--- scripts/generate-certificates.sh
+++ scripts/generate-certificates.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout charts/code-editor/tls.key -out charts/code-editor/tls.crt -subj "/C=XX/ST=Italy/L=Empoli/O=SUSE/OU=ECM/CN=code-editor"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout charts/code-editor/tls.key -out charts/code-editor/tls.crt -subj "/C=XX/ST=Italy/L=Empoli/O=SUSE/OU=ECM/CN=code-editor" 2>/dev/null
 
-echo "Self-signed certificate successfully generated."
+echo "Self-signed certificate successfully generated"
