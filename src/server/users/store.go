65ad5af6ec5757c663f2fc2bdb973d48577250d7
Francesco Torchia <francesco.torchia@suse.com>
2024-01-06 12:59:26 +0100

    Implement User Store delete
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- src/server/users/store.go
+++ src/server/users/store.go
@@ -17,6 +17,7 @@ type storeI interface {
 	List()
 	Get()
 	Set()
+	Del()
 }
 
 type store struct {
@@ -43,8 +44,8 @@ func (store store) List() []models.User {
 	return utils.Slice[models.User](_store)
 }
 
-func (store store) Get(id string) (models.User, bool) {
-	return _store[id], _store[id] != models.User{}
+func (store store) Get(username string) (models.User, bool) {
+	return _store[username], _store[username] != models.User{}
 }
 
 func (store store) Set(user models.User) {
@@ -75,6 +76,42 @@ func (store store) Set(user models.User) {
 	}
 
 	_store[user.Name] = user
+}
+
+func (store store) Del(username string) {
+	secret, err := k.Clientset.CoreV1().Secrets(c.Config.App.Namespace).Get(context.TODO(), c.Config.Resources.UsersName, metav1.GetOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	var users []models.User
+	err = yaml.Unmarshal(secret.Data["users"], &users)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	for i := range users {
+		if username == users[i].Name {
+			users = append(users[:i], users[i+1:]...)
+			break
+		}
+
+	}
+
+	_byte, err := yaml.Marshal(users)
+	if err != nil {
+		panic(err)
+	}
+
+	secret.Data = make(map[string][]byte)
+	secret.Data["users"] = _byte
+
+	_, err = k.Clientset.CoreV1().Secrets(c.Config.App.Namespace).Update(context.TODO(), secret, metav1.UpdateOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	delete(_store, username)
 }
 
 var Store = store{}
